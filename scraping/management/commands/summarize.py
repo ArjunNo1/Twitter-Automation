02094e8d6b9fd9bd74a0356dad31a735bcb1e485
Arjun Balaji <95229197+ArjunNo1@users.noreply.github.com>
2022-10-20 14:43:13 +0530

    Add files via upload

---
+++ scraping/management/commands/summarize.py
@@ -0,0 +1,61 @@
+
+from turtle import pos
+from django.core.management.base import BaseCommand
+from scraping.models import Post
+from transformers import pipeline
+import pandas as pd
+from scraping.models import Post
+
+
+class Command(BaseCommand):
+    summarizer = pipeline("summarization")
+    help = "collect jobs"
+    # define logic of command
+
+    def ht(self,text):
+        t = text.split(" ")
+        tex = []
+        h = []
+        # print(t)
+        for i in t:
+            if "#" in i:
+                h.append(i)
+            else:
+                tex.append(i)
+        #   hat.append(" ".join(h))
+        return " ".join(tex), " ".join(h)
+
+    def summ(self,text):
+        s, h = self.ht(text)
+        res = self.summarizer(s, max_length=100, min_length=30, do_sample=False)
+        # res+=h.split(" ")
+        # print(type(res))
+        # print(h)
+        return res[0]['summary_text'] + '\n' + h
+
+
+    def handle(self, *args, **options):
+        
+
+        posts = Post.objects.all()
+        post_dataframe = pd.DataFrame.from_records(posts.values())
+        print(post_dataframe.head())
+
+        post_dataframe['summarized_text'] = post_dataframe['text'].apply(lambda x: self.summ(x))
+        # post_dataframe.loc[0, 'summarized_text'] = post_dataframe.iloc[0,:].apply(self.summ)
+        
+
+        for i in range(len(post_dataframe)):
+            Post.objects.filter(id=post_dataframe.loc[i,'id']).update(
+                summarized_text = post_dataframe.loc[i, 'summarized_text']
+            )
+        print(post_dataframe.loc[0, 'summarized_text'])
+
+
+
+
+# df['ns'] = df[sum'nt'].apply(lambda x: ht(x))
+# df['hat'] = pd.DataFrame(hat)
+# res = summarizer(df['ns'], max_length=130, min_length=30, do_sample=False)[0]['summary_text']
+        self.stdout.write( 'job complete' )
+        
---
+++ scraping/models.py
@@ -0,0 +1,27 @@
+from email.policy import default
+from django.db import models
+# import dao
+from django.utils import timezone
+class Post(models.Model):
+    # lst=[]
+    # url = models.CharField(max_length=250, unique=True)
+    dateposted = models.CharField(max_length = 200)
+    text = models.TextField()
+    summarized_text = models.TextField(default = "NS")
+    likes = models.PositiveIntegerField()  
+
+    created_date = models.DateTimeField(default=timezone.now)
+    # lst.append(dateposted)
+    # lst.append(text)
+    # lst.append(likes)
+    # lst.append(created_date)
+    def __str__(self):
+        return self.text[:10]
+    class Meta:
+        ordering = ['dateposted']
+    class Admin:
+        pass
+    # dao.insertData(lst,tablename)
+    # Post('public.tweet')
+
+# Create your models here.
